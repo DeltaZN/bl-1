aa2ad1777bdbf88711042855d35ac6062d6f85cd
Georgii Savin <gsavin@devexperts.com>
2021-03-24 21:24:28 +0300

    wildfly support

--- src/main/kotlin/ru/itmo/bllab1/BlLab1Application.kt
+++ src/main/kotlin/ru/itmo/bllab1/BlLab1Application.kt
@@ -3,28 +3,31 @@ package ru.itmo.bllab1
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.security.crypto.password.PasswordEncoder
 import ru.itmo.bllab1.auth.ERole
 import ru.itmo.bllab1.repository.*
+import org.springframework.boot.builder.SpringApplicationBuilder
 
 @SpringBootApplication
-class BlLab1Application(
-	private val roleRepository: RoleRepository,
-	private val userRepository: UserRepository,
-	private val encoder: PasswordEncoder,
-) : CommandLineRunner {
-	override fun run(vararg args: String?) {
-		val roleBorrower = Role(name = ERole.ROLE_BORROWER)
-		val roleBorrowerConfirmed = Role(name = ERole.ROLE_BORROWER_CONFIRMED)
-		val roleManager = Role(name = ERole.ROLE_MANAGER)
-		val roleAdmin = Role(name = ERole.ROLE_ADMIN)
-		roleRepository.save(roleAdmin)
-		roleRepository.save(roleManager)
-		roleRepository.save(roleBorrower)
-		roleRepository.save(roleBorrowerConfirmed)
-		val admin = EUser(0, "admin", encoder.encode("666666"), null, null, setOf(roleAdmin))
-		userRepository.save(admin)
+class BlLab1Application : SpringBootServletInitializer() {
+
+	override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder? {
+		return application.sources(BlLab1Application::class.java)
 	}
+
+//	override fun run(vararg args: String?) {
+//		val roleBorrower = Role(name = ERole.ROLE_BORROWER)
+//		val roleBorrowerConfirmed = Role(name = ERole.ROLE_BORROWER_CONFIRMED)
+//		val roleManager = Role(name = ERole.ROLE_MANAGER)
+//		val roleAdmin = Role(name = ERole.ROLE_ADMIN)
+//		roleRepository.save(roleAdmin)
+//		roleRepository.save(roleManager)
+//		roleRepository.save(roleBorrower)
+//		roleRepository.save(roleBorrowerConfirmed)
+//		val admin = EUser(0, "admin", encoder.encode("666666"), null, null, setOf(roleAdmin))
+//		userRepository.save(admin)
+//	}
 }
 
 fun main(args: Array<String>) {
